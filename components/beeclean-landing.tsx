8b6d57523f46a1fccea36f401ab68d9e6ba961ec
Ubaidullah Ismail <abdulahadismail.dev@gmail.com>
2026-03-22 02:56:49 -0700

    Initial commit from Create Next App

---
+++ components/beeclean-landing.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { motion } from 'motion/react';
+
+const BeeCleanLanding = () => {
+  return (
+    <div className="beeclean-landing">
+      <Header />
+      <HeroSection />
+      <FeaturesSection />
+      <HowItWorks />
+      <SocialProof />
+      <FAQSection />
+      <FinalCTA />
+      <Footer />
+    </div>
+  );
+};
+
+export default BeeCleanLanding;
+"
